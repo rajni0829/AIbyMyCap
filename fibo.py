d3d5ccf283df08b4b65189346e34a48a84163bb2
Rajni Gupta <rajnigupta2901@gmail.com>
2021-12-06 20:44:00 +0530

    Fibonacci Series added

---
+++ fibo.py
@@ -0,0 +1,20 @@
+# Fibonacci Series   -0,1,1,2,3,4,5,...
+
+nterms = int(input("Enter number of terms "))
+n1, n2 = 0, 1
+count = 0
+
+if nterms <= 0:
+   print("Please enter a positive integer")
+
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+
+else:
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
