e4994a85961990148db8444d29a9e069ae90bb16
Rajni Gupta <rajnigupta2901@gmail.com>
2021-12-08 18:34:58 +0530

    Set Operations added!

---
+++ setOperations.py
@@ -0,0 +1,8 @@
+# Set Operations
+E = {8,0,2,4,6}
+N = {1,3,2,4,5}
+
+print("Union of E and N is ",E.union(N))
+print("Intersection of E and N is ",E.intersection(N))
+print("Difference of of E and N is ",E.difference(N))
+print("Symmetric difference of of E and N is ",E.symmetric_difference(N))
